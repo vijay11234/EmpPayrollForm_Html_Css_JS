c67d9003ba4585e555ad8084379fd861ee1a4db4
vijay11234 <victory6923@gmail.com>
2020-12-17 23:28:37 +0530

    [vijay kumar: performed validation employee payroll data setter methods

--- Js/EmployeePayroll.js
+++ Js/EmployeePayroll.js
@@ -1,58 +1,82 @@
-class EmployeePayRoll {
-    //we are adding this id to use it in the form by which we will update delete and find values
-    get id() { return this._id; }
-    set(id) {
-        this._id = id;
+class EmployeePayroll {
+
+    constructor(...params) {
+        this.name = params[0];
+        this.picture = params[1];
+        this.gender = params[2];
+        this.department = params[3];
+        this.salary = params[4];
+        this.startDate = params[5];
+        this.notes = params[6];
     }
 
-    get name() { return this._name; }
+    get name() {
+        return this._name;
+    }
+
     set name(name) {
-        //validating only name with min 3 letters and first letter caps is allowed  
-        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$');
-        if (nameRegex.test(name))
-            this._name = name;
-        else throw 'Name is Incorrect!';
+        let nameRegex=RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+        if(nameRegex.test(name))
+            this._name=name;
+        else throw 'Entered Name is Incorrect ';
+    }
+       
+
+    get picture() {
+        return this._picture;
+    }
+    set picture(picture) {
+        this._picture = picture;
     }
 
-    get profilePic() { return this._profilePic; }
-    set profilePic(profilePic) {
-        this._profilePic = profilePic;
+    get gender() {
+        return this._gender;
     }
 
-    get gender() { return this._gender; }
     set gender(gender) {
         this._gender = gender;
     }
 
-    get department() { return this._department; }
+    get department() {
+        return this._department;
+    }
+
     set department(department) {
         this._department = department;
     }
 
-    get salary() { return this._salary; }
+    get salary() {
+        return this._salary;
+    }
+
     set salary(salary) {
         this._salary = salary;
     }
 
-    get note() { return this._note; }
-    set note(note) {
-        this._note = note;
+    get startDate() {
+        return this._startDate;
     }
 
-    get startDate(){ return this._startDate;}
-    set startDate(startDate){
-        this._startDate = startDate;
+    set startDate(startDate) {
+
+        if (startDate<= new Date())
+            this._startDate = startDate;
+        else throw "Incorrect start date";
+        
     }
 
+    get notes() {
+        return this._notes;
+    }
+
+    set notes(notes) {
+        this._notes = notes;
+    }
+
+    //method
     toString() {
-        //giving the type of date required
-        const options = { year: 'numeric', month: 'short', day: 'numeric' };
-        //if start date is not in the given datatype it will pass the value undefined to empdate
-        const empDate = !this.startDate ? "undefined" :
-            this.startDate;
-        //here in return for start date we are passing empDate as value                
-        return "id=" + this.id + ", name='" + this.name + ", gender='" + this.gender +
-            ", profilePic='" + this.profilePic + ", department=" + this.department +
-            ", salary=" + this.salary + ", startDate=" + empDate + ", note=" + this.note;
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = !this.startDate ? "undefined" : this.startDate.toLocalDateString("en-US", options);
+        return name=" + this.name + ", gender=" + this.gender + ", picture=" + this.picture + ", department=" + this.department +", salary=" + this.salary + ", empDate=" + this.startDate + ", notes=" + this.notes";
     }
 }
