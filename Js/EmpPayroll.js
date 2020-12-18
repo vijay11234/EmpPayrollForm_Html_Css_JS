05536a9b281c5f0463a2452328e06c8c769aa341
vijay11234 <victory6923@gmail.com>
2020-12-18 05:44:25 +0530

    [vijay kumar]: adding event listeners when documnet is loaded

--- Js/EmpPayroll.js
+++ Js/EmpPayroll.js
@@ -1,19 +1,23 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function () {
+        if (name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;;
+            textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
+});
+
 const salary = document.getElementById("salary");
 const output = document.getElementById("salary-output");
 output.innerHTML = salary.value;
 salary.oninput = function () {
     output.innerHTML = this.value;
 }
-
-const text = document.querySelector('.input');
-const textError = document.querySelector('.text-error');
-text.addEventListener('input', function () {
-    let nameRegex = RegExp(/^([\w]{3,})+\s+([\w\s]{3,})+$/);
-    //Checking name correct or incorrect using if else condition
-    if (nameRegex.test(text.value)) {
-        textError.textContent = "";
-    }
-    else {
-        textError.textContent = "Entered name is incorrect";
-    }
-});
