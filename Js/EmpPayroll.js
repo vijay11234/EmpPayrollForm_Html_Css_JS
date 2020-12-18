5bc6b24e5a4116451c75ff8c265638420c9e3905
vijay11234 <victory6923@gmail.com>
2020-12-18 22:04:56 +0530

    [vijay kumar]: adding saved Employee payroll object in local storage

--- Js/EmpPayroll.js
+++ Js/EmpPayroll.js
@@ -18,9 +18,21 @@ window.addEventListener('DOMContentLoaded', (event) => {
 const save = () => {
     try {
         let employeePayrollData = createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData)
     } catch (e) {
         return;
     }
+}
+
+function createAndUpdateStorage(employeePayrollData) {
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    if(employeePayrollList != undefined){
+        employeePayrollList.push(employeePayrollData);
+    } else{
+        employeePayrollList = [employeePayrollData]
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList", JSON.stringyfy(employeePayrollList))
 }
 
 const createEmployeePayroll = () => {
