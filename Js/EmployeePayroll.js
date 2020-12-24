950e6b6a7eb1e4a6760a6fde182d3fc460a04604
vijay11234 <victory6923@gmail.com>
2020-12-24 14:54:12 +0530

    [vijay kumar] refacoring code

--- Js/EmployeePayroll.js
+++ Js/EmployeePayroll.js
@@ -1,44 +1,30 @@
 class EmployeePayrollData {
-    
-    //getter and setter method
-    get id() { 
-        return this._id; 
+    get id() {
+        return this._id;
     }
-
-    set id(id) { 
+    set id(id) {
         this._id = id;
     }
-
     get name() {
         return this._name;
     }
-
     set name(name) {
-        let nameRegex=RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
-        if(nameRegex.test(name))
-            this._name=name;
-        else throw ' Name is Incorrect ';
+        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+        if (nameRegex.test(name))
+            this._name = name;
+        else throw "Name is incorrect!";
     }
 
-    get picture() {
-        return this._picture;
+    get profilePic() {
+        return this._profilePic;
     }
-    set picture(picture) {
-        this._picture = picture;
-    }
-
-    get gender() {
-        return this._gender;
-    }
-
-    set gender(gender) {
-        this._gender = gender;
+    set profilePic(profilePic) {
+        this._profilePic = profilePic;
     }
 
     get department() {
         return this._department;
     }
-
     set department(department) {
         this._department = department;
     }
@@ -46,35 +32,47 @@ class EmployeePayrollData {
     get salary() {
         return this._salary;
     }
-
     set salary(salary) {
-        this._salary = salary;
+        let salaryRegex = RegExp('^[1-9]{1}[0-9]*$');
+        if (salaryRegex.test(salary))
+            this._salary = salary;
+        else throw "Salary should be non zero positive number";
+    }
+
+    get gender() {
+        return this._gender;
+    }
+    set gender(gender) {
+        if (gender != undefined) {
+            let genderRegex = RegExp('^(Male|Female)$');
+            if (genderRegex.test(gender)) {
+                this._gender = gender;
+            } else {
+                throw "Gender incorrect";
+            }
+        }
+    }
+
+    get note() {
+        return this._note;
+    }
+    set note(note) {
+        this._note = note;
     }
 
     get startDate() {
         return this._startDate;
     }
-
     set startDate(startDate) {
-        if (startDate<= new Date())
-        this._startDate = startDate;
-    else throw "Incorrect start date";
-}
-        
-    
-
-    get note() {
-        return this._note;
+        let now = new Date();
+        if(startDate > now) throw 'Start date is a Future Date!';
+        var diff = Math.abs(now.getTime() - startDate.getTime());
+        if (diff / (1000 * 60 * 60 * 24) > 30)
+            // throw 'Start Date is beyond 30 days';
+        this._startDate = startDate;  
     }
 
-    set notes(note) {
-        this._note = note;
-    }
-
-    //method
     toString() {
-        const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        const empDate = !this.startDate ? "undefined" : this.startDate.toLocalDateString("en-US", options);
-        return "id=" + this.id + ", name=" + this.name + ", gender=" + this.gender + ", picture=" + this.picture + ", department=" + this.department +", salary=" + this.salary + ", empDate=" + this.startDate + ", note=" + this.note;
+        return "ID: " + this.id + " Name: " + this.name + " Salary: " + this.salary + " Gender: " + this.gender + " Start Date: " + this.startDate + "Department: " + this.department + "Profile Pic: " + this.profilePic + "Note: " + this.note;
     }
 }
